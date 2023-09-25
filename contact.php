0051d4c4d6d5e06a7f9c47e4f229370ec98d5925
LeThiNhat <145972628+LeThiNhat@users.noreply.github.com>
2023-09-25 21:49:27 +0700

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,61 +1,143 @@
-<?php 
- include 'inc/header.php';
- include 'inc/slider.php';
- ?>
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
 
- <div class="main">
-    <div class="content">
-    	<div class="support">
-  			<div class="support_desc">
-  				<h3>Live Support</h3>
-  				<p><span>24 hours | 7 days a week | 365 days a year &nbsp;&nbsp; Live Technical Support</span></p>
-  				<p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-  			</div>
-  				<img src="web/images/contact.png" alt="" />
-  			<div class="clear"></div>
-  		</div>
-    	<div class="section group">
-				<div class="col span_2_of_3">
-				  <div class="contact-form">
-				  	<h2>Contact Us</h2>
-					    <form>
-					    	<div>
-						    	<span><label>NAME</label></span>
-						    	<span><input type="text" value=""></span>
-						    </div>
-						    <div>
-						    	<span><label>E-MAIL</label></span>
-						    	<span><input type="text" value=""></span>
-						    </div>
-						    <div>
-						     	<span><label>MOBILE.NO</label></span>
-						    	<span><input type="text" value=""></span>
-						    </div>
-						    <div>
-						    	<span><label>SUBJECT</label></span>
-						    	<span><textarea> </textarea></span>
-						    </div>
-						   <div>
-						   		<span><input type="submit" value="SUBMIT"></span>
-						  </div>
-					    </form>
-				  </div>
-  				</div>
-				<div class="col span_1_of_3">
-      			<div class="company_address">
-				     	<h2>Company Information :</h2>
-						    	<p>500 Lorem Ipsum Dolor Sit,</p>
-						   		<p>22-56-2-9 Sit Amet, Lorem,</p>
-						   		<p>USA</p>
-				   		<p>Phone:(00) 222 666 444</p>
-				   		<p>Fax: (000) 000 00 00 0</p>
-				 	 	<p>Email: <span>info@mycompany.com</span></p>
-				   		<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
-				   </div>
-				 </div>
-			  </div>    	
+<?php include '../classes/contact.php'; ?>
+<?php
+$con = new contact();
+?>
+
+<div class="grid_10">
+    <div class="box round first grid">
+        <h2>Thông tin liên hệ</h2>
+
+        <div class="block">
+            <?php
+            $getContactInfo = $con->getContactInfo();
+            if (isset($getContactInfo)) {
+                while ($result = $getContactInfo->fetch_assoc()) {
+                    ?>
+
+                    <table class="table table-hover">
+                        <thead>
+                            <!-- <tr>
+                                <th scope="col">Tiêu đề</th>
+                                <td scope="col">Nội dung</td>
+                            </tr> -->
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Điện thoại</th>
+                                <td>
+                                    <?php echo $result['phone'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Địa chỉ</th>
+                                <td>
+                                    <?php echo $result['address'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Email</th>
+                                <td>
+                                    <?php echo $result['email'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Vận chuyển</th>
+                                <td>
+                                    <?php echo $result['transport'] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Ghi chú</th>
+                                <td>
+                                    <?php echo $result['note'] ?>
+                                </td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <a class="edit-button" style="text-align: center;" href="contactedit.php?<?php echo $result['id'] ?>">Sửa</a>
+
+
+                    <?php
+                }
+            }
+            ?>
+
+
+
+            <style>
+                /* Style the table */
+                .table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+
+                /* Style table header */
+                .table th {
+                    background-color: #f2f2f2;
+                    text-align: left;
+                    padding: 10px;
+                }
+
+                /* Style table rows */
+                .table td {
+                    border: 1px solid #e1e1e1;
+                    padding: 10px;
+                }
+
+                /* Style alternating rows */
+                .table tbody tr:nth-child(even) {
+                    background-color: #f9f9f9;
+                }
+
+                /* Style table header text */
+                .table th {
+                    font-weight: bold;
+                }
+
+                /* Style table cell text */
+                .table td {
+                    font-weight: normal;
+                }
+
+                /* Style the edit button */
+                .edit-button {
+                    display: inline-block;
+                    padding: 10px 20px;
+                    /* Adjust padding as needed */
+                    background-color: #007BFF;
+                    /* Background color for the button */
+                    color: #fff;
+                    /* Text color */
+                    text-align: center;
+                    text-decoration: none;
+                    border-radius: 4px;
+                    /* Rounded corners */
+                    transition: background-color 0.3s;
+                    /* Smooth background color transition */
+                }
+
+                /* Hover effect for the button */
+                .edit-button:hover {
+                    background-color: #0056b3;
+                    /* New background color on hover */
+                }
+            </style>
+
+        </div>
+
     </div>
- </div>
- <?php
- include'inc/footer.php';
- ?>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        setupLeftMenu();
+
+        $('.datatable').dataTable();
+        setSidebarHeight();
+    });
+</script>
+<?php include 'inc/adminfooter.php';?> 
